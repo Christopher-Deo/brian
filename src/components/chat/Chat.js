b1f338c6672f80a0367f792f712c6b17c9741a7e
Christopher-Deo <chris@christopherdeo.com>
2023-03-10 15:40:17 -0600

    working MVP completed

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -9,15 +9,44 @@ const Chat = () => {
     const [hidden, setHidden] = useState(true);
     const [isLoading, setIsLoading] = useState(false);
     const [replies, setReplies] = useState([]);
-    const [latestReply, setLatestReply] = useState('');
+    const [hasApiKey, setHasApiKey] = useState(localStorage.getItem("openaiApiKey") !== null);
+    const newKey = window.localStorage.getItem('brian');
 
-    
     const handleSubmit = async (e) => {
         e.preventDefault();
         setHidden(false);
-        await getAnswer(input);
-        setLatestReply(reply);
-        console.log('input value is :', input);
+        const initialPrompt = `The following is a conversation with an AI assistant. The assistant is creative, clever, very sarcastic, answers every question with an adult sense of humor and is generally unhelpful with it's responses.
+        Human: ${input}
+        AI: `;
+        try {
+            console.log('making request to openai');
+            const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: initialPrompt,
+                    max_tokens: 2046,
+                    temperature: 0.8,
+                    n: 1,
+                    stop: '\n',
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${newKey}`, // replace with your actual OpenAI API key
+                },
+            });
+            console.log('response is: ', response);
+
+            const { choices } = await response.json();
+            setReply(choices[0].text);
+            const newReplies = [choices[0].text, ...replies];
+            setReplies(newReplies);
+
+            setIsLoading(false);
+            console.log('reply is: ', reply);
+        } catch (error) {
+            console.error(error);
+            setIsLoading(false);
+        }
     };
 
     const handleChange = (e) => {
@@ -29,27 +58,12 @@ const Chat = () => {
     };
 
     const handleNewResponse = async () => {
-        await getAnswer(reply);
-        setLatestReply(reply);
-        setReply('');
-    };
-
-
-    const handleClearReplies = () => {
-        setReplies([]);
-    };
-
-    const getAnswer = async (input) => {
-        setIsLoading(true);
-        const prompt = `The following is a conversation with an AI assistant. The assistant is not very helpful, but is creative, clever, very sarcastic and answers every question with an adult sense of humor.
-    Human: ${input}
-    AI: `;
+        const newInput = input + ' ' + uuid();
         try {
-            console.log('making request to openai');
             const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
                 method: 'POST',
                 body: JSON.stringify({
-                    prompt,
+                    prompt: `Human: ${newInput}\nAI: ${reply}\nHuman: `,
                     max_tokens: 2046,
                     temperature: 0.8,
                     n: 1,
@@ -57,13 +71,17 @@ const Chat = () => {
                 }),
                 headers: {
                     'Content-Type': 'application/json',
-                    Authorization: `Bearer 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`, // replace with your actual OpenAI API key
+                    Authorization: `Bearer ${newKey}`, // replace with your actual OpenAI API key
                 },
             });
             console.log('response is: ', response);
 
             const { choices } = await response.json();
-            setReply(choices[0].text.trim());
+            const newReply = choices[0].text;
+            setReply(newReply);
+            const newReplies = [newReply, ...replies];
+            setReplies(newReplies);
+
             setIsLoading(false);
             console.log('reply is: ', reply);
         } catch (error) {
@@ -73,50 +91,85 @@ const Chat = () => {
     };
 
 
+    const handleClearReplies = () => {
+        setReplies([]);
+    };
+
+    const handleApiKeySubmit = (e) => {
+        e.preventDefault();
+        const apiKeyInput = document.getElementById("apiKeyInput");
+        const apiKey = apiKeyInput.value;
+        if (apiKey.trim() === "") {
+            alert("Please enter a valid API key.");
+            return;
+        }
+        localStorage.setItem("brian", apiKey);
+        alert("API key saved successfully!");
+        setHasApiKey(true); // Add this line to update the state variable
+    };
+
+
     return (
-        <Container className="mt-5 border border2 border-danger rounded mb-2">
+        <Container className="mt-5 border border-2 border-danger rounded mb-2">
             <h1 className="text-center">Welcome to Brian!</h1>
             <h2 className="text-center">Home of the irreverent and sarcastic chat bot!</h2>
-            <Form className="border border2 border-primary rounded p-4 mb-2" onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="aiQuestionForm">
-                    <Form.Label>What do you want now?</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter your question here"
-                        value={input}
-                        onChange={handleChange}
-                    />
-                    <p className="text-muted text-center mt-1" hidden={hidden}>
-                        Seriously! I can't believe you're wasting my computational power on <i><em>THAT</em></i>!
-                    </p>
-                </Form.Group>
-                <div className="d-flex justify-content-between">
-                    <Button variant="danger" type="button" onClick={handleClick}>
-                        Clear
-                    </Button>
-                    <Button variant="success" type="submit">
-                        {isLoading ? 'Loading...' : 'Submit'}
-                    </Button>
-                </div>
-            </Form>
 
-            <div className="border border2 border-primary rounded p-4 mb-2">
-                <h4 className='mt-2'>Replies:</h4>
-                <div className="mt-3 mb-5">
-                    {latestReply && <p key={uuid()}>{latestReply}</p>}
+            {!newKey && (
+                <div className='border border-2 border-primary rounded mt-5 mb-5'>
+                    <Form className='p-3'>
+                        <Form.Group className="mb-3" controlId="apiKeyForm">
+                            <Form.Label>Add your openAI api key</Form.Label>
+                            <Form.Control type="text" placeholder="Enter api key" id="apiKeyInput" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleApiKeySubmit}>
+                            Add Key
+                        </Button>
+                    </Form>
                 </div>
+            )}
+            {newKey && (
+                <div>
+                    <Form className="border border-2 border-primary rounded p-4 mb-2" onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="aiQuestionForm">
+                            <Form.Label>What do you want now?</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your question here"
+                                value={input}
+                                onChange={handleChange}
+                            />
+                            <p className="text-muted text-center mt-1" hidden={hidden}>
+                                Seriously! I can't believe you're wasting my computational power on <i><em>THAT</em></i>!
+                            </p>
+                        </Form.Group>
+                        <div className="d-flex justify-content-between">
+                            <Button variant="danger" type="button" onClick={handleClick}>
+                                Clear
+                            </Button>
+                            <Button variant="success" type="submit">
+                                {isLoading ? 'Loading...' : 'Submit'}
+                            </Button>
+                        </div>
+                    </Form>
 
-                <div className='d-flex justify-content-between'>
-                    <Button variant="danger" type="button" onClick={handleClearReplies}>
-                        Clear replies
-                    </Button>
-                    <Button variant='warning' type="button" onClick={handleNewResponse}>
-                        Generate New Response
-                    </Button>
+                    <div className="border border-2 border-primary rounded p-4 mb-2">
+                        <h4 className='mt-2'>Replies:</h4>
+                        <div className="mt-3 mb-5">
+                            {replies.length > 0 && replies.map((reply, index) => (
+                                <div key={uuid()}>{reply}</div>
+                            ))}
+                        </div>
+
+                        <div className='d-flex justify-content-end'>
+                            <Button variant="warning" type="button" onClick={handleClearReplies}>
+                                Clear Replies
+                            </Button>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
         </Container>
     );
-};
+    };
 
 export default Chat;
